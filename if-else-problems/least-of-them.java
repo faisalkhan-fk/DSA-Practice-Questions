bfe4a080535a5e93114347d70db3ddb31499ad3a
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-01 22:16:09 +0530

    Add least of three numbers program in Java
    Implemented a program to find the least of three positive integers.

---
+++ if-else-problems/least-of-them.java
@@ -0,0 +1,27 @@
+//Take 3 positive integers input and print the least of them.
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter 1st Number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter 2nd Number: ");
+        int b=sc.nextInt();
+        System.out.print("Enter 3rd Number: ");
+        int c=sc.nextInt();
+        if(a<b){
+          if(a<c){
+              System.out.println(a);
+          }else{
+              System.out.println(c);
+          } 
+        }else{
+             if(b<c){
+                 System.out.println(b);
+             }else{
+                 System.out.println(c);
+             } 
+        }
+    }
+}
