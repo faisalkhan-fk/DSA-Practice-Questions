a9296edb7f87594b33f56b61f6a09f8972c1a2be
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-02 12:07:36 +0530

    Add Java program to calculate square of a number

---
+++ Basics-of-DSA/Square-of-number.java
@@ -0,0 +1,11 @@
+//square of a number
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter the Number: ");
+      int n=sc.nextInt();
+      int square=n*n;
+      System.out.println("Square of the Number is: "+square);
+    }
+}
