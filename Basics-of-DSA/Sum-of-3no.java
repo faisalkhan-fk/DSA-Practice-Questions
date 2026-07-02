f0f10d570991332af8e0015eeab9686fc0f63002
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-02 12:14:40 +0530

    Add Java program to calculate sum of three numbers

---
+++ Basics-of-DSA/Sum-of-3no.java
@@ -0,0 +1,16 @@
+//Take 3 numbers input and print their sum
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter 1st Number: ");
+		int a=sc.nextInt();
+        System.out.print("Enter 2nd Number: ");
+        int b=sc.nextInt();
+        System.out.print("Enter 3rd Number: ");
+        int c=sc.nextInt();
+		    int sum=a+b+c;
+		System.out.println("Sum of the Numbers is: "+sum);
+	}
+}
