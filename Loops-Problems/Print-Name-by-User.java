57937836df093d78fd41d575cde7455a73b56c7e
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-05 08:54:07 +0530

    Implement user input to print name multiple times
    Added functionality to print 'Yashika' n times based on user input.

---
+++ Loops-Problems/Print-Name-by-User.java
@@ -0,0 +1,12 @@
+//Print _Yashika_ ‘n’ times. Take ‘n’ input from user
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+        System.out.println("Yashika");
+        }
+    }
+}
