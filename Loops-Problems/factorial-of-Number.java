173ca716dbf763c8d7e1a347038fc5ae905f71f7
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-07 12:23:37 +0530

    Add factorial calculation program in Java

---
+++ Loops-Problems/factorial-of-Number.java
@@ -0,0 +1,14 @@
+//Factorial of a number
+import java.util.*;
+    public class Main{
+        public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        System.out.print("Factrial of a Number: "+fact);
+    }
+}
