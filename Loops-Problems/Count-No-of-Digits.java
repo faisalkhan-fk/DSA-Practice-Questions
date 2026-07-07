4f76d04f1ab0033c4a92022c94eabfe706c14c8f
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-07 09:56:56 +0530

    Add digit counting program in Count-No-of-Digits.java
    Implement a Java program to count the number of digits in a given number using a while loop.

---
+++ Loops-Problems/Count-No-of-Digits.java
@@ -0,0 +1,15 @@
+//Count digits of a number
+import java.util.*;
+    public class Main{
+        public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        int count=0;
+        while(n!=0){
+            n/=10;
+            count++;
+        }
+        System.out.print("Number of Digits: "+count);
+    }
+}
