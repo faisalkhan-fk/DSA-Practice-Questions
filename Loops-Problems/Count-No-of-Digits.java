a287053c615f4d31a2b396da495461d474617897
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-07 10:05:41 +0530

    Fix digit count for input number zero
    Handle case where input number is zero by setting count to 1.

--- Loops-Problems/Count-No-of-Digits.java
+++ Loops-Problems/Count-No-of-Digits.java
@@ -5,6 +5,7 @@ import java.util.*;
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the Number: ");
         int n=sc.nextInt();
+        if(n==0) n=1;
         int count=0;
         while(n!=0){
             n/=10;
