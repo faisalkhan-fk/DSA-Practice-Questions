e7b41467ca2153a8411e179f674c3fe76d9a33b7
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 11:22:35 +0530

    Create Print-Pattern-1111.java

---
+++ Pattern-Printing/Print-Pattern-1111.java
@@ -0,0 +1,20 @@
+/*Print the given pattern
+Number Square 2
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4*/
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
