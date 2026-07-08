22c5f4e173cc623f072e7d033f69173ffa533ee2
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 20:35:01 +0530

    Implement hollow rectangle pattern printing in Java
    Added a Java program to print a hollow rectangle pattern based on user-defined dimensions.

---
+++ Pattern-Printing/Print-pattern-Hollow-Rectangle.java
@@ -0,0 +1,27 @@
+/*Print the given pattern
+* * * * * * *
+*           *
+*           *
+*           *
+* * * * * * *     */
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the rows: ");
+        int m=sc.nextInt();
+        System.out.print("Enter the Column: ");
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==m || j==1 || j==n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
