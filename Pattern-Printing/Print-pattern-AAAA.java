6ce3f801636e3996b5746759478ca8b38697ac5f
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 11:28:04 +0530

    Refactor pattern printing to accept user input
    Updated the pattern printing program to include user input for the size of the alphabet square.

---
+++ Pattern-Printing/Print-pattern-AAAA.java
@@ -0,0 +1,20 @@
+/*Print the given pattern
+Alphabet Square 3
+A A A A
+B B B B
+C C C C
+D D D D*/
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
