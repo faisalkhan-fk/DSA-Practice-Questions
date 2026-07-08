4a818e18d0f0805425fea8d80e6db8f9b33f4a0d
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 20:53:07 +0530

    Implement plus pattern printing in Java
    Added a Java program to print a plus pattern based on user input.

---
+++ Pattern-Printing/Print-Pattern-(plus).java
@@ -0,0 +1,28 @@
+/*Print the given pattern
+       *     
+       *
+ * * * * * * *
+       * 
+       *                       */
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Column: ");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.print("Please Enter Odd Number: ");
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==n/2+1 || j==n/2+1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
