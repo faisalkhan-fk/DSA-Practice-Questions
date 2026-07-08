2e5791facdb7c01c50a51a182ba97767a6c076b5
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 09:23:36 +0530

    Implement rectangle pattern printing in Java
    Add a Java program to print a rectangle pattern of asterisks.

--- Pattern-Printing/Print-Rectangle.java
+++ Pattern-Printing/Print-Rectangle.java
@@ -1,1 +1,15 @@
-
+/*Print the given pattern * * * * *
+                          * * * * *
+                          * * * * *
+                          * * * * *                                                */
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+    for(int i=1;i<=3;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
