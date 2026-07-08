7b246b8cbbde1d8f33db379899aa4e497a46028d
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 22:37:16 +0530

    Add binary triangle pattern printing program

---
+++ Pattern-Printing/Print-Pattern-binary-triangle.java
@@ -0,0 +1,19 @@
+//Print pattern Binary Triangle
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Size: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1+" ");
+                }else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+    }
+}
+}   
