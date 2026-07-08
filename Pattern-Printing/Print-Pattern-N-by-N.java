e968160cd4142b0a5e9da8536c153795a3700cfe
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-08 09:46:38 +0530

    Modify pattern printing to use user-defined size

--- Pattern-Printing/Print-Pattern-N-by-N.java
+++ Pattern-Printing/Print-Pattern-N-by-N.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the Number of Rows: ");
-        int row=sc.nextInt();
-        for(int i=1;i<=row;i++){
-            for(int j=1;j<=row;j++){
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
                 System.out.print("* ");
             }
             System.out.println();
