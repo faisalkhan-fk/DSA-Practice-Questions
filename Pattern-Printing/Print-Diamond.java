21ff4caf6e350e4a39665770747b3ca55e80e86c
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-09 19:08:57 +0530

    Add Print-Diamond.java for diamond pattern printing
    Implement a Java program to print a diamond pattern based on user input.

---
+++ Pattern-Printing/Print-Diamond.java
@@ -0,0 +1,35 @@
+//Print Pattern of Diamond
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        int nsp=n-1, nst=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=nst;j++){
+                System.out.print("* ");
+            }
+            nsp-=1; 
+            nst+=2;
+            System.out.println();
+        }
+            
+            nsp=1; 
+            nst=(2*n-3);
+            for(int i=1;i<=n-1;i++){
+            for(int j=1;j<=nsp;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=nst;j++){
+                System.out.print("*"+" ");
+            }
+            nsp++;
+            nst-=2;
+            System.out.println();
+        }
+    }
+}
