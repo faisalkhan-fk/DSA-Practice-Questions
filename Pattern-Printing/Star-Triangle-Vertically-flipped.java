13d21c831e98e24c3cbed5ed3a92234fa4db6066
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-09 15:27:10 +0530

    Add vertically flipped star triangle pattern program
    This program prints a vertically flipped star triangle pattern based on user input.

---
+++ Pattern-Printing/Star-Triangle-Vertically-flipped.java
@@ -0,0 +1,19 @@
+//Print Pattern Star Triangle Vertically
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if((i+j-1)>=n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
