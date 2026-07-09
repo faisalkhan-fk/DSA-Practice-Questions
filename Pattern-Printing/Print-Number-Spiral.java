d1c936cf32067c8b501202cfc0af59c226d8cdf6
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-09 20:46:55 +0530

    Add Print-Number-Spiral.java for number spiral pattern

---
+++ Pattern-Printing/Print-Number-Spiral.java
@@ -0,0 +1,19 @@
+//Print Number Spiral
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n=sc.nextInt();
+        
+            for(int i=1;i<=(2*n-1);i++){
+            for(int j=1;j<=(2*n-1);j++){
+                int a=i, b=j;
+                if(i>n) a=(2*n-i);
+                if(j>n) b=(2*n-j);
+               System.out.print(((a<b)? a: b)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
