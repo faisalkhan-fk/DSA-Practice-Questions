7c67bd59eb42e7d06d36d57a03a652fa16c7397d
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-12 22:06:30 +0530

    Implement element printing in array

---
+++ Arrays-Problems/Element-Printing.java
@@ -0,0 +1,8 @@
+//Print Index Element in Array
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int[] x={2,54,65,44,33,55};
+        System.out.println(x[5]);
+    }
+}
