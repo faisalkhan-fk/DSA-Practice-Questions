7cc070843f6c105106c8fb0443619071a2c7ea14
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-14 15:29:28 +0530

    Add Java program to calculate sum of array elements

---
+++ Arrays-Problems/Sum-of-elements-of-Array.java
@@ -0,0 +1,19 @@
+// Print sum of elements of the array.
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Size of Array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int sum=0;
+        System.out.print("Enter the Elements of the Array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        System.out.println("Sum of the Elements is: "+sum);
+    }
+}
