9e91d390e521aeb7ff0ede04c4ea55a639cc4a3c
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-15 22:56:08 +0530

    Add Java program to search for an element in array

---
+++ Arrays-Problems/Search-Element-In-Array.java
@@ -0,0 +1,29 @@
+// Search Element in Array
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the target value: ");
+        int target=sc.nextInt();
+        System.out.print("Enter the size of Array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        boolean flag=false; //flag false means targest Element does not exists in Array
+        System.out.print("Enter the Elements of the Array: ");
+        for(int i=0;i<n;i++){
+          arr[i]=sc.nextInt();  
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i] == target){
+                flag=true; //flag true means Element exists in Array
+                break;
+            }
+        }
+       if(flag==true){
+           System.out.println("Target exists in Array");
+       }else{
+           System.out.println("Target does not exists in Array");
+       }
+    }
+}
