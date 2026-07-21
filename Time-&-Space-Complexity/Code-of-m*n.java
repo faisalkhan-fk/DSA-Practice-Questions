15078f2d38e0d441ca84efc92161e09fb146efbb
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-21 22:17:53 +0530

    Add program to print in n*m grid format
    Implemented a program to print 'Faisal Khan' in an n*m grid format, with time complexity O(m*n).

---
+++ Time-&-Space-Complexity/Code-of-m*n.java
@@ -0,0 +1,18 @@
+//Write the program of n*m rows and column and calculate the Complexity of the code
+//Time complexity of this code is- O(m*n)
+
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the Number of Rows: ");
+    int n=sc.nextInt();
+    System.out.print("Enter the Number of columns: ");
+    int m=sc.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+        System.out.println("Faisal Khan"+" ");
+    }
+    }
+}
+}    
