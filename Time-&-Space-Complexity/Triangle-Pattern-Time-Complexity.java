caa8e755c603aa5758b2cdd5ab49727bab5db3a5
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-22 21:02:04 +0530

    Add triangle pattern printing Java program
    Implement a triangle pattern printing program with time complexity analysis.

---
+++ Time-&-Space-Complexity/Triangle-Pattern-Time-Complexity.java
@@ -0,0 +1,13 @@
+//Print the tringle pattern 
+//Time complexity-O(n)
+
+public class Main{
+    public static void main(String[] args){
+        for(int i=1; i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
