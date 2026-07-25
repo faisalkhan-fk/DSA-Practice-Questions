26f4c69d0d46e1c0495e6a1467aada1e69726597
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-25 18:02:25 +0530

    Add PassingArrayToMethods class to modify array
    Implement a Java program to demonstrate passing an array to a method and modifying its contents.

---
+++ Arrays-Problems/Passing-Array-to-method.java
@@ -0,0 +1,12 @@
+//Passing Array to method in java
+public class PassingArrayToMethods {
+    public static void main(String[] args) {
+        int x[] = {10,3,29,38};
+        System.out.println(x[2]);
+        change(x);
+        System.out.println(x[2]);
+    }
+    public static void change(int[] y) {
+        y[2] = 99;
+    }
+}
