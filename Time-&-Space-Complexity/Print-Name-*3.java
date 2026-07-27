ab7106bad2c21152699ec4c43edf729b49861b20
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-27 16:53:04 +0530

    Fix loop initialization in Print-Name-*3.java

--- Time-&-Space-Complexity/Print-Name-*3.java
+++ Time-&-Space-Complexity/Print-Name-*3.java
@@ -2,7 +2,7 @@
 
 public class Main{
     public static void main(String[] args){
-        for(int i=2;i<=10;i*=3){
+        for(int i=1;i<=10;i*=3){
                 System.out.print("Vinay ");
             }
     }
