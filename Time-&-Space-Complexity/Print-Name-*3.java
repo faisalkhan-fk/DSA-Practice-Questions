d0f78785348ae83476d6ad3320d56b4e0e6a28c6
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-27 16:52:09 +0530

    Add Print-Name-*3.java to demonstrate time complexity
    Implement a Java program that prints 'Vinay' three times using a loop with a time complexity analysis.

---
+++ Time-&-Space-Complexity/Print-Name-*3.java
@@ -0,0 +1,9 @@
+//Printin name =*3 and find the time complexity
+
+public class Main{
+    public static void main(String[] args){
+        for(int i=2;i<=10;i*=3){
+                System.out.print("Vinay ");
+            }
+    }
+}
