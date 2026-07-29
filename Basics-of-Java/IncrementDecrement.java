23e035ff127dd4ca5065e33a9e893055a7c4c43f
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-29 22:31:33 +0530

    Add IncrementDecrement.java file

---
+++ Basics-of-Java/IncrementDecrement.java
@@ -0,0 +1,9 @@
+//Increment Decrement in Java
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = ++x;
+        System.out.println(x+" "+y);
+
+    }
+}
