53344b74d09d3d6444a9842575bfcf5ca9b48406
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-30 14:55:11 +0530

    Add Java program to demonstrate time complexity
    Implement a simple Java program to print 'GFG' while i*i is less than or equal to 5.

---
+++ Time-&-Space-Complexity/i*i.java
@@ -0,0 +1,9 @@
+//Time Complexity of this program is √n
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        for(int i=1;i*i<=5;i++){
+            System.out.print("GFG ");
+        }
+    }
+}
