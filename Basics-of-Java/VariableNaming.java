2adb150595fdfad113d68800aa22d242badc00dc
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-07 23:06:01 +0530

    Add comments on variable naming in Java
    Added comments about variable naming conventions in Java.

---
+++ Basics-of-Java/VariableNaming.java
@@ -0,0 +1,9 @@
+//Variable Naming in Java
+public class Main {
+    public static void main(String[] args) {
+        // _ or alphabet or $
+        // ! @ # $ % ^ & * ( ) _ - + = ~ ` [ ] { } \ | : ; ' " < > , . / ?
+        int xy = 3;
+        int raghav = 5;
+    }
+}
