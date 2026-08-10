aa8f1c4675ed048eae1b255ac1fb9b939209a6b3
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-10 22:29:49 +0530

    Add Print-Pattern-W.java to print 'W' pattern
    Implement a Java program to print the pattern 'W'.

---
+++ Pattern-Printing/Print-Pattern-W.java
@@ -0,0 +1,16 @@
+//Print the pattern W
+public class Main{
+    public static void main(String[] arg){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+               if(j==1 || j==n || (i+j==n+1 && i!=2) || (i==j && i!=2)){
+                   System.out.print("* ");
+              }else{
+                   System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
