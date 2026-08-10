ecd8115e028af590557b608837c081a1a95c5345
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-10 23:07:41 +0530

    Change pattern printing from 'B' to 'F'

---
+++ Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-F.java
@@ -0,0 +1,17 @@
+//Print the pattern Alphabate F
+public class Main{
+    public static void main(String[] arg){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=4;j++){
+               if(i==1 || j==1 || i==3){
+                   System.out.print("* ");
+              }else{
+                   System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
+
