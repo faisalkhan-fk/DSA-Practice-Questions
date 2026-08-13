3d39f6b6e7de14dc09b1384f5419f71dd9b932a9
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-13 23:29:52 +0530

    Add Java program to print 'S' pattern
    Implement a Java program to print the letter 'S' pattern using asterisks.

---
+++ Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-S.java
@@ -0,0 +1,17 @@
+//Print the pattern Alphabate S
+//Print pattern diagonal
+public class Main{
+    public static void main(String[] arg){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+               if(i==1 || i==n || i==3 || (j==1 && i!=n-1) || (j==n && i!=2)){
+                   System.out.print("* ");
+               }else{
+                   System.out.print("  ");
+               }
+            }
+            System.out.println();
+        }
+    }
+}
