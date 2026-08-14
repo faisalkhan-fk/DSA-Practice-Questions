ae046ed0c4b7fffe606c2f1587a7eab896e94e00
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-15 00:30:24 +0530

    Add pattern printing for alphabet 'K' in Java
    Implemented a Java program to print the pattern of the alphabet 'K'.

---
+++ Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-K.java
@@ -0,0 +1,18 @@
+//Print the pattern Alphabate K
+public class Main{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==2 || 
+                   (i<=n/2+1 && i+j==n+1) || 
+                   (i>=n/2+1 && i==j)){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
