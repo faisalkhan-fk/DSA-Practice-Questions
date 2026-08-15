b420911ec17110cbdc0a55ad83258ef0d9baf2cc
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-15 20:25:04 +0530

    Update pattern size from 6 to 5
    Changed the variable 'n' from 6 to 5 to adjust the pattern size.

--- Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-O.java
+++ Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-O.java
@@ -1,7 +1,7 @@
 //Print the pattern Alphabate O
 public class Main{
     public static void main(String[] arg){
-        int n=6;
+        int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=4;j++){
                if((j==1 && i!=1 && i!=n) || (j==4 && i!=1 && i!=n) 
