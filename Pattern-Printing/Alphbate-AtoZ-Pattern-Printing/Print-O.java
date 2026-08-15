c7ad4fd34a87ea9a1d67564bf9ef0e327bbb3aa6
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-15 20:23:42 +0530

    Modify pattern printing for letter 'O' to height 6
    Updated the pattern printing logic for letter 'O' to include a new height of 6 and adjusted the conditions for printing stars.

--- Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-O.java
+++ Pattern-Printing/Alphbate-AtoZ-Pattern-Printing/Print-O.java
@@ -1,13 +1,15 @@
 //Print the pattern Alphabate O
 public class Main{
     public static void main(String[] arg){
-        int n=5;
+        int n=6;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=4;j++){
-               if((j==1) || j==4 || i==1 || i==n){
+               if((j==1 && i!=1 && i!=n) || (j==4 && i!=1 && i!=n) 
+               || (i==1 && j!=1 && j!=4) 
+               || (i==n && j!=1 && j!=4)){
                    System.out.print("* ");
               }else{
-                   System.out.print("  ");
+                   System.out.print(" ");
                }
             }
             System.out.println();
