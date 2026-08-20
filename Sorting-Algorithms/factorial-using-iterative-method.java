93218ba3b930766d9701d5d7143d339eabb80ff5
Faisal Khan <faisalkhan7863030@gmail.com>
2026-08-20 21:50:55 +0530

    Add iterative method for calculating factorial

---
+++ Sorting-Algorithms/factorial-using-iterative-method.java
@@ -0,0 +1,27 @@
+//factorial using iterative method
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long fact = 1;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+            count++;
+        }
+
+        System.out.println("Factorial is: " + fact);
+        System.out.println("Count is: " + count);
+    }
+}
+//Output
+// Enter a number: 5
+// Factorial is: 120
+// Count is: 5
