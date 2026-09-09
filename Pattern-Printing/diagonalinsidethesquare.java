5fe8559aea65d7a2d4fa4b5a07fe2de22dd22095
Faisal Khan <faisalkhan7863030@gmail.com>
2026-09-09 23:51:11 +0530

    Add diagonal inside square pattern printing

---
+++ Pattern-Printing/diagonalinsidethesquare.java
@@ -0,0 +1,16 @@
+//Diagonal Inside the Square
+public class Main{
+    public static void main(String[] args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==n+1-i || i==j || i==1 ||j==1 || j==n || i==n){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
