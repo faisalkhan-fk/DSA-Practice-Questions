df5ace586f139d494ab8713262630ee4393b58b0
Faisal Khan <faisalkhan7863030@gmail.com>
2026-09-16 21:34:49 +0530

    Add Interview-asked-question.java file

---
+++ Pattern-Printing/Interview-asked-question.java
@@ -0,0 +1,14 @@
+//Interview Asked question on 16th September
+public class Main {
+    public static void main(String[] args) {
+
+        int N = 4;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
